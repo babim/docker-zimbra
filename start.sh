097491aaae9ff790c342b47d8e72aabcf543a76d
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-01-08 11:49:07 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+service ssh start
+rm -f /opt/zimbra/zmstat/pid/zmstat-*.pid
+su -c "/opt/zimbra/bin/zmloggerctl restart" zimbra
+/opt/zimbra/libexec/zmsyslogsetup
+kill -9 $(ps ax | grep rsyslog | grep -v "grep" | awk '{print $1}')
+rsyslogd -f /etc/rsyslog.conf
+su -c "/opt/zimbra/libexec/zmstatuslog restart" zimbra
+su -c "/opt/zimbra/bin/zmstatctl restart" zimbra
+/opt/zimbra/libexec/zmsyslogsetup
+su -c "/opt/zimbra/bin/zmupdateauthkeys restart" zimbra
+su -c "/opt/zimbra/bin/zmcontrol restart" zimbra
+su -c "/opt/zimbra/bin/zmcontrol start" zimbra
+su -c "/opt/zimbra/bin/zmcontrol status" zimbra
+bash
