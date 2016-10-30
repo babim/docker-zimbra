05e31a0f641fcde0f9f00ed8e7cfaabf22acb72c
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-10-30 21:47:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-chown root:root /var/run/sshd
-service ssh restart
 # Set DNS Server to localhost
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
