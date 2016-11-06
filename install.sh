be0efe4039a3ce8e7b382b0498f729c897e2fe2a
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-06 16:31:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,31 @@ AdminPort1=${AdminPort:-7071}
 AdminURL1=${AdminURL:-/zimbraAdmin}
 PASSWORD1=${PASSWORD:-123456}
 
+# SSH
+if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
+    echo "=> Found authorized keys"
+    mkdir -p /root/.ssh
+    chmod 700 /root/.ssh
+    touch /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+    IFS=$'\n'
+    arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
+    for x in $arr
+    do
+        x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
+        cat /root/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "=> Adding public key to /root/.ssh/authorized_keys: $x"
+            echo "$x" >> /root/.ssh/authorized_keys
+        fi
+    done
+fi
+
+# set password root is root
+SSHPASS1=${SSHPASS:-root}
+echo "root:$SSHPASS1" | chpasswd
 service ssh start
+
 ## Installing the DNS Server ##
 echo "Installing DNS Server"
 #sudo apt-get update && sudo sudo apt-get install -y bind9 bind9utils bind9-doc dnsutils
